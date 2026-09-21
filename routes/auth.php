5e5d175957d3a6f9b977f63976fed71b2a76b61b
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2026-09-21 17:49:51 +0200

    update

--- routes/auth.php
+++ routes/auth.php
@@ -13,10 +13,10 @@ use App\Http\Controllers\Front\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('register', [HomeController::class, 'home'])
                 ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('register', [HomeController::class, 'home']);
 /* 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login'); */
