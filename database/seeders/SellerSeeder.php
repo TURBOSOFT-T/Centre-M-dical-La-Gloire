862466c38a39f48ab5023d1f5056f6655ad1964b
TURBOSOFT <42888421+TURBOSOFT-T@users.noreply.github.com>
2026-09-21 03:18:37 +0200

    update

--- database/seeders/SellerSeeder.php
+++ database/seeders/SellerSeeder.php
@@ -17,64 +17,131 @@ class SellerSeeder extends Seeder
         'dashboard',
         'clients_view',
         'clients_delete',
+
         'category_view',
         'category_add',
         'category_edit',
         'category_delete',
-        'trasport_view',
-        'trasport_add',
-        'trasport_edit',
-        'trasport_delete',
-        'code_promotion_view',
-        'code_promotion_add',
-        'code_promotion_edit',
-        'code_promotion_delete',
-        'compte_view',
-        'compte_add',
-        'compte_edit',
-        'compte_delete',
-        'ajouter_solde_view',
-        'ajouter_solde_add',
-        'ajouter_solde_edit',
-        'ajouter_solde_delete',
+
         'marque_view',
         'marque_add',
         'marque_edit',
         'marque_delete',
+
+        'consultation_view',
+        'consultation_add',
+        'consultation_edit',
+        'consultation_dlete',
+
+        'boutique_view',
+        'boutique_add',
+        'boutique_edit',
+        'boutique_delete',
+
+        'visiteur_view',
+        'visiteur_add',
+        'visiteur_edit',
+        'visiteur_delete',  
+
+        'assurance_view',
+        'assurance_add',
+        'assurance_edit',   
+        'assurance_delete',
+
+        'patient_view',
+        'patient_add',
+        'patient_edit',
+        'patient_delete',
+
+        'rendez-vous_view',
+        'rendez-vous_add',
+        'rendez-vous_edit',
+        'rendez-vous_delete',
+
+        'rapport_view',
+        'rapport_add',
+        'rapport_edit',
+        'rapport_delete',
+
+
+
+
+
+     
+
+        'famille_view',
+        'famille_add',
+        'famille_edit',
+        'famille_delete',
+
+        'trasport_view',
+        'trasport_add',
+        'trasport_edit',
+        'trasport_delete',
+
+        'code_promotion_view',
+        'code_promotion_add',
+        'code_promotion_edit',
+        'code_promotion_delete',
+
+        'compte_view',
+        'compte_add',
+        'compte_edit',
+        'compte_delete',
+
+        'ajouter_solde_view',
+        'ajouter_solde_add',
+        'ajouter_solde_edit',
+        'ajouter_solde_delete',
+
+        'marque_view',
+        'marque_add',
+        'marque_edit',
+        'marque_delete',
+
         'sponsor_view',
-        'sposor_edit',
         'sposor_add',
+        'sposor_edit',
         'sponsor_delete',
+
         'service_view',
         'service_add',
         'service_edit',
         'service_delete',
+
         'product_view',
         'product_add',
         'product_edit',
         'product_delete',
+
         'coupon_view',
         'coupon_add',
         'coupon_edit',
         'coupon_delete',
+
         'testimonial_view',
         'testimonial_add',
         'testimonial_edit',
         'testimonial_delete',
+
         'table_view',
         'table_add',
         'table_edit',
         'table_delete',
+
         'price_view',
+
         'order_view',
         'order_add',
         'order_edit',
         'order_delete',
+
         'my_order_view',
         'live_order_view',
         'live_order_add',
         'live_order_edit',
         'live_order_delete',
+
         'setting_view',
         'message_view',
         'gestion_stock'
